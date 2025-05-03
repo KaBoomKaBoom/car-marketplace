5c8524483bdd56bc994698e963250f6f68b17f90
Berco Andrei <120446308+KaBoomKaBoom@users.noreply.github.com>
2025-05-03 20:35:21 +0300

    Feat: Light/dark mode
    * Add custom colors for theme in tailwind config
    
    * Modify index.html for my project
    
    * Add tailwind to index.css
    
    * Add theme management in local storage
    
    * Add theme management(bg/text color changing) + 2 pages TBD

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1D4ED8', // Blue for buttons, nav
+        secondary: '#9333EA', // Purple for secondary actions
+        accent: '#F59E0B', // Amber for highlights
+        backgroundLight: '#F3F4F6', // Light gray for light mode
+        backgroundDark: '#1F2937', // Dark gray for dark mode
+        textLight: '#111827', // Dark text for light mode
+        textDark: '#D1D5DB', // Light text for dark mode
+      },
+    },
   },
+  darkMode: 'class', // Enables class-based dark mode
   plugins: [],
 }
-
